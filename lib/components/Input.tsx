06f1552d8f13c317b602d3c4e87f509e6cc7f2e0
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2023-10-24 11:00:18 -0500

    Tweak Input

--- lib/components/Input.tsx
+++ lib/components/Input.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>{
-    label: string,
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    label?: string,
+    labelClassName?: string,
+    inputClassName?: string,
 }
 
-const inputStyles = {
-    base: 'eb-block eb-w-full eb-pl-2 eb-shadow-inner eb-border-0 eb-py-1.5 eb-rounded-none eb-text-gray-900 eb-ring-1 eb-ring-inset eb-ring-gray-300 placeholder:eb-text-gray-400 sm:eb-text-sm sm:eb-leading-6',
-}
+const inputStyles = 'eb-block eb-w-full eb-pl-2 eb-shadow-inner eb-border-0 eb-py-1.5 eb-rounded-none eb-text-gray-900 eb-ring-1 eb-ring-inset eb-ring-gray-300 placeholder:eb-text-gray-400 sm:eb-text-sm sm:eb-leading-6';
 
-export default function Input(props: InputProps) {
+const labelStyles = "eb-block eb-text-sm eb-font-medium eb-leading-6 eb-text-gray-900";
+
+export default function Input({
+    label,
+    labelClassName = '',
+    inputClassName = '',
+    ...props
+}: InputProps) {
     return (
         <>
-            <label htmlFor={props.id} className="eb-block eb-text-sm eb-font-medium eb-leading-6 eb-text-gray-900">
-                {props.label}
-            </label>
-            <div className="eb-mt-2">
+            <div className={props.className}>
+                {
+                    label && (
+                        <label
+                            htmlFor={props.id}
+                            className={`${labelStyles} ${labelClassName}`}>
+                            {label}
+                        </label>
+                    )
+                }
                 <input
                     {...props}
-                    className={inputStyles.base}
+                    className={`${inputStyles} ${inputClassName}`}
                 />
             </div>
         </>
