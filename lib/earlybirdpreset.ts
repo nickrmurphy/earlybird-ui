ed434869be4ed4e905138105ab921c156d9ddae1
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2023-10-24 16:32:20 -0500

    Update Preset to Extend

--- lib/earlybirdpreset.ts
+++ lib/earlybirdpreset.ts
@@ -2,12 +2,13 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
     theme: {
-        fontFamily: {
-            serif: ['Noto Serif', ...defaultTheme.fontFamily.serif],
-            mono: ['Noto Mono', ...defaultTheme.fontFamily.mono],
-            display: ['Noto Serif Display', ...defaultTheme.fontFamily.sans],
+        extend: {
+            fontFamily: {
+                serif: ['Noto Serif', ...defaultTheme.fontFamily.serif],
+                mono: ['Noto Mono', ...defaultTheme.fontFamily.mono],
+                display: ['Noto Serif Display', ...defaultTheme.fontFamily.sans],
+            },
         },
-        extend: {},
     },
     plugins: [],
 }
